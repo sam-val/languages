417f0b0b24ef84c7eee3b0955d7090e4f466b301
sam-val <samvalentine2811@gmail.com>
2023-03-13 00:41:10 +0700

    init crud/go

---
+++ crud_movie/go/db/migrations/000001_init_tables.up.sql
@@ -0,0 +1,51 @@
+CREATE TABLE "Movie" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "year" int NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "Filmmaker" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "dob" date NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "Role" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "MovieCredits" (
+  "id" bigserial PRIMARY KEY,
+  "filmmaker_id" bigint NOT NULL,
+  "movie_id" bigint NOT NULL,
+  "role_id" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "Movie" ("name");
+
+CREATE INDEX ON "Filmmaker" ("name");
+
+CREATE INDEX ON "Role" ("name");
+
+CREATE INDEX ON "MovieCredits" ("filmmaker_id");
+
+CREATE INDEX ON "MovieCredits" ("movie_id");
+
+CREATE INDEX ON "MovieCredits" ("role_id");
+
+CREATE INDEX ON "MovieCredits" ("filmmaker_id", "movie_id");
+
+CREATE UNIQUE INDEX ON "MovieCredits" ("filmmaker_id", "movie_id", "role_id");
+
+COMMENT ON COLUMN "Role"."name" IS 'director, actor, etc';
+
+ALTER TABLE "MovieCredits" ADD FOREIGN KEY ("filmmaker_id") REFERENCES "Filmmaker" ("id");
+
+ALTER TABLE "MovieCredits" ADD FOREIGN KEY ("movie_id") REFERENCES "Movie" ("id");
+
+ALTER TABLE "MovieCredits" ADD FOREIGN KEY ("role_id") REFERENCES "Role" ("id");
---
+++ crud_movie/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/sam-val/languages/crud_movie/go
+
+go 1.20
